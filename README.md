3a5b7aa97a6d029c743171545e857e1781f0050e
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-06-08 20:50:51 -0500

    Postman: HTTP Requests GET, POST, PUT, DELETE, PATCH

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Follow me: [Instagram](https://www.instagram.com/elisperezmusic)
 ### Server code skeleton.
 
 #### Add public folder.
+- Middlewares: use.static
+
+#### Postman: HTTP Requests
+- GET
+- POST
+- PUT
+- DELETE
+- PATCH
--- models/server.js
+++ models/server.js
@@ -13,7 +13,29 @@ class Server {
   // methods
   routes() {
     this.app.get('/api', (req, res) => {
-      res.send('Hello world');
+      res.json({
+        msg: 'get API'
+      });
+    });
+    this.app.put('/api', (req, res) => {
+      res.json({
+        msg: 'put API'
+      });
+    });
+    this.app.post('/api', (req, res) => {
+      res.json({
+        msg: 'post API'
+      });
+    });
+    this.app.delete('/api', (req, res) => {
+      res.json({
+        msg: 'delete API'
+      });
+    });
+    this.app.patch('/api', (req, res) => {
+      res.json({
+        msg: 'patch API'
+      });
     });
   }
 
