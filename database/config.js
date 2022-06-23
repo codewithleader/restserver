3ca472fbe17146d05badb2598e72823d36c77236
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-06-23 13:44:31 -0500

    other fix

--- database/config.js
+++ database/config.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const dbConnection = async () => {
+const dbConnection = async() => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+  });
     console.log('MongoDB connected');
   } catch (error) {
     console.log(error);
