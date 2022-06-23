1551d3999ec444c5fb7d463712d2a2eeed357669
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-06-23 14:06:44 -0500

    Before connecting to the database, we need to check if the cluster IP in MongoDB Atlas is 0.0.0.0/0

--- database/config.js
+++ database/config.js
@@ -1,17 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// ? Before connecting to the database, we need to check if the cluster IP in MongoDB Atlas is 0.0.0.0/0
 const dbConnection = async() => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-  });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (error) {
-    console.log(error);
+    console.error(error);
     throw new Error('Error connecting to database');
   }
 };
