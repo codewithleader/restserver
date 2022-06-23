cfdef77a30b6c9f72c903df2d3ade9494b48c369
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-06-23 12:48:07 -0500

    fix

--- middlewares/validate-fields.js
+++ middlewares/validate-fields.js
@@ -1,13 +1,11 @@
 const { validationResult } = require('express-validator');
 
 const validateResult = (req, res, next) => {
-  try {
-    validationResult(req).throw();
-    return next();
-  } catch (error) {
-    res.status(400);
-    res.send({ msg: error.array() });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors);
   }
+  next();
 };
 
 module.exports = { validateResult };
