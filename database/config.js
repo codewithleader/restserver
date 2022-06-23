c6175b19884809ac739e9f05c266db3679b14623
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-06-23 13:54:50 -0500

    mongoose downgraded

--- database/config.js
+++ database/config.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 
 const dbConnection = async() => {
   try {
-    mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
   });
     console.log('MongoDB connected');
   } catch (error) {
